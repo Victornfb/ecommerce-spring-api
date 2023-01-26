38b94f12795f9247a563051fbd859ad0ddfffea3
Victor Frigatto <victornfb@outlook.com>
2023-01-26 15:35:59 -0300

    Feat: Add get doctors route

--- src/main/java/voll/med/api/controller/MedicoController.java
+++ src/main/java/voll/med/api/controller/MedicoController.java
@@ -2,15 +2,18 @@ package voll.med.api.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import voll.med.api.endereco.Endereco;
 import voll.med.api.medico.DadosCadastroMedico;
+import voll.med.api.medico.DadosListagemMedico;
 import voll.med.api.medico.Medico;
 import voll.med.api.medico.MedicoRepository;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -27,6 +30,16 @@ public class MedicoController {
         DadosCadastroMedico dados
     ) {
         this.repository.save(new Medico(dados));
+    }
+
+    @GetMapping
+    public Page<DadosListagemMedico> listar(
+        @PageableDefault(size = 10, sort = {"nome"})
+        Pageable paginacao
+    ) {
+        return this.repository
+            .findAll(paginacao)
+            .map(DadosListagemMedico::new);
     }
 
 }
